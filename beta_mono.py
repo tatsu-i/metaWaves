fcac1480bd1c0d34b059906efd5156f2a611f9d9
tatsui <2360691+tatsu-i@users.noreply.github.com>
2022-06-13 20:57:40 +0900

    add new beat

---
+++ beta_mono.py
@@ -0,0 +1,28 @@
+from AccelBrainBeat.brainbeat.monaural_beat import MonauralBeat
+from pydub import AudioSegment
+import numpy as np
+import wave_func as wf
+import os
+
+total_time_min = 30
+total_time = 60*total_time_min
+
+file_name = "/tmp/beta.wav"
+brain_beat = MonauralBeat()
+
+brain_beat.save_beat(
+    output_file_name="/tmp/1.wav",
+    frequencys=(446, 482),
+    play_time=total_time,
+    volume=0.3
+)
+brain_beat.save_beat(
+    output_file_name="/tmp/2.wav",
+    frequencys=(567, 607),
+    play_time=total_time,
+    volume=0.3
+)
+sound1 = AudioSegment.from_file("/tmp/1.wav")
+sound2 = AudioSegment.from_file("/tmp/2.wav")
+output = sound1.overlay(sound2, position=0)
+output.export("beta_mono.wav", format="wav")
